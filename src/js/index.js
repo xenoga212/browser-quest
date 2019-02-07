2d878a7c2cd39b3762544731c8bc9113d98d334e
Angel Figueroa <afigueroa742@gmail.com>
2019-02-07 17:36:22 -0500

    html basics done; starting on player module

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import Player from './models/Player';
+
+const player = new Player('sora', 'warrior');
+
+console.log(player);
---
+++ src/js/models/Player.js
@@ -0,0 +1,14 @@
+console.log('Working on player');
+export default class Player {
+    constructor(name, specialty) {
+        this.name = name;
+        this.specialty = specialty;
+        this.stats = {
+            strength: 0,
+            magic: 0,
+            defense: 0,
+            dexterity: 0,
+            health: 10
+        }
+    }
+}
