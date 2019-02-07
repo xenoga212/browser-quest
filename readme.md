0c3e5326f06c98bcb676a121f7e9cb2c1c96a7d9
Angel Figueroa <afigueroa742@gmail.com>
2019-02-06 19:56:02 -0500

    webpack and babel files created and customized

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Project Title
+
+Browser Quest is a browser based RPG game. Find your way out of the tower and claim your freedom
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const config = {
+    // Reload on File Change
+    watch: true,
+    // entry
+    entry: ['./src/js/index.js', './src/sass/main.scss'],
+    
+    // output
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+    },
+
+    // Loaders
+    module: {
+        rules: [
+            // Javascript files
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            
+            // CSS files
+
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader', 
+                    'postcss-loader',
+                    'sass-loader'
+                ],
+            }
+        ]
+    },
+
+    // plugins
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            hash: true
+        }),
+
+        new webpack.LoaderOptionsPlugin({ 
+            options: {
+                postcss: [
+                    autoprefixer()
+                ]
+            }
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].bundle.css'
+        })
+
+    ]
+
+   
+};
+
+module.exports = config;
